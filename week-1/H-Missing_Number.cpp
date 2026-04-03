af14dd4e7db1a6fe5b17be92a47bb003677afffd
mdnaim2004 <naim.cse2004@gmail.com>
2026-04-04 00:26:27 +0600

    Add initial implementations for H-Missing_Number and Increasing_Array problems

---
+++ week-1/H-Missing_Number.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long int n;
+    cin >> n;
+
+    long long int x;
+    long long int sum = 0;
+
+    for(int i=0; i<n-1; i++){
+        cin >> x;
+        sum += x;
+    }
+
+    long long int total = n*(n+1)/2;
+
+    long long int find = total - sum;
+
+    cout << find << "\n";
+
+    return 0;
+}
