e6ed7902afaebc07f560140c99c63f553c7d6d64
mdnaim2004 <naim.cse2004@gmail.com>
2026-04-04 13:53:16 +0600

    Refactor AT_ABC_220_A solution to iterate through range and find first divisible number

--- week-1/AT_ABC_220_A_LANG_EN.cpp
+++ week-1/AT_ABC_220_A_LANG_EN.cpp
@@ -4,9 +4,13 @@ using namespace std;
 int main(){
     int a, b, c;
     cin >> a >> b >> c;
-    if(a < c && b>c)
-        cout << (c) <<"\n";
-    else
-        cout << -1 << "\n";
+
+    for(int i=a; i<=b; i++){
+        if(i % c == 0){
+            cout << i << endl;
+            return 0;
+        }
+    }
+    cout << -1 << "\n";
     return 0;
 }
