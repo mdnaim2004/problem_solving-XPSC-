0818b93d110d2564fdfd48e21f09c8428185b425
mdnaim2004 <naim.cse2004@gmail.com>
2026-04-05 08:11:29 +0600

    Fix formatting of vector declaration in Increasing_Array.cpp

--- week-1/Increasing_Array.cpp
+++ week-1/Increasing_Array.cpp
@@ -5,7 +5,7 @@ int main(){
     long long int n;
     cin >> n;
     
-    vector<long long int>arr(n);
+    vector<long long int> arr(n);
     for(long long int i=0; i<n; i++)
         cin >> arr[i];
 
