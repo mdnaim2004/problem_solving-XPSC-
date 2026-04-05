6a1abfd966ab5425c97451b58709387ca93cfa9b
mdnaim2004 <naim.cse2004@gmail.com>
2026-04-05 08:03:49 +0600

    Fix data type consistency in Increasing_Array implementation

--- week-1/Increasing_Array.cpp
+++ week-1/Increasing_Array.cpp
@@ -6,12 +6,12 @@ int main(){
     cin >> n;
     
     vector<long long int>arr(n);
-    for(int i=0; i<n; i++)
+    for(long long int i=0; i<n; i++)
         cin >> arr[i];
 
     long long int moves = 0;
 
-    for(int i=1; i<n; i++){
+    for(long long int i=1; i<n; i++){
         if(arr[i] < arr[i-1]){
             moves += arr[i-1]-arr[i];
             arr[i] = arr[i-1];
