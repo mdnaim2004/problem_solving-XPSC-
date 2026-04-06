39befaff8e5386b953b3d2ee0f3032b6c84ffaa6
Md Naim <naim.cse2004@gmail.com>
2026-04-06 21:54:46 +0600

    Refactor main function and improve formatting
    Fix formatting issues and clean up code.

--- week-1/AT_ABC_220_A_LANG_EN.cpp
+++ week-1/AT_ABC_220_A_LANG_EN.cpp
@@ -1,10 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 int main(){
     int a, b, c;
     cin >> a >> b >> c;
-
     for(int i=a; i<=b; i++){
         if(i % c == 0){
             cout << i << endl;
