2bcd5456adf93cd05976e0dab5e5ec93752076c3
mdnaim2004 <naim.cse2004@gmail.com>
2026-04-07 23:19:58 +0600

    Add solution for AT_ABC_071_B problem

---
+++ week-1/AT_ABC_071_B_LANG_EN.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    cin>>s;
+    bool fnd = false;
+
+    for(char c= 'a'; c <= 'z'; c++){
+        bool fond = false;
+        for(int i=0; i<s.size(); i++){
+            if(s[i] == c){
+                fond = true;
+                break;
+            }
+        }
+
+        if(fond == false){
+            cout << c;
+            return 0;
+        }
+    }
+    cout << "None" << "\n";
+    return 0;
+}
