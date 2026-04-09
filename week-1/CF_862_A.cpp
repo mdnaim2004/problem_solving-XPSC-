0ddd5a3a8e8f3c532f3c2b146b7d08e05ca7740a
mdnaim2004 <naim.cse2004@gmail.com>
2026-04-09 17:55:33 +0600

    Fix logic for counting zeros in CF 862 A problem solution

--- week-1/CF_862_A.cpp
+++ week-1/CF_862_A.cpp
@@ -10,7 +10,12 @@ int main(){
     for(int i=0; i<s; i++){
         cin >> arr[i];
     }
-
+    int ans = 0;
     
+    for(int i=0; i<x; i++){
+        if(arr[i] == 0)
+            ans++;
+    }
+
     return 0;
 }
