f428032ec57f1e217374d28d9f50e1accfd780ea
mdnaim2004 <naim.cse2004@gmail.com>
2026-04-09 17:29:01 +0600

    Add initial implementation for CF 862 A problem definition and source file

---
+++ week-1/CF_862_A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int s, x;
+    cin >> s >> x;
+
+    vector<int> arr(s);
+
+    for(int i=0; i<s; i++){
+        cin >> arr[i];
+    }
+
+    
+    return 0;
+}
