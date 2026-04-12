6f378a22832f6c96f785a2ca00b9c615eb00c520
mdnaim2004 <naim.cse2004@gmail.com>
2026-04-12 10:51:53 +0600

    Add implementation for CF 2218 D problem solution

--- codeforces-1  div-4/CF_2218_D.cpp
+++ codeforces-1  div-4/CF_2218_D.cpp
@@ -1,1 +1,25 @@
-  
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector<long long int> arr;
+        long long int a = 1, b = 3;
+
+        while(arr.size() < n){
+            arr.push_back(a*b);
+            a += 2;
+            b += 2;
+
+        }
+        for(auto x : arr){
+            cout << x << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
