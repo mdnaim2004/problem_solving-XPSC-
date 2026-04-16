add7341561908f54c562e75a49f431fa0792a102
mdnaim2004 <naim.cse2004@gmail.com>
2026-04-16 20:11:49 +0600

    this problem, find the minimum  subarray, to convert the all list of black to white

--- STL-1 Week 2/CF_1927_A.cpp
+++ STL-1 Week 2/CF_1927_A.cpp
@@ -11,17 +11,6 @@ int main(){
         string s;
         cin >> s;
 
-        int st = s.find('B');
-        int end = s.rfind('B');
-        
-
-        // for(int i=0; i<n; i++){
-        //     if(s[i] == 'B' && s[i] == 0)
-        //         st++;
-        //     if(s[i] == 'B' && s[i] != 0)
-        //         end++;
-        // }
-        cout << abs(st-end)+1 << "\n";
         
     }
     return 0;
