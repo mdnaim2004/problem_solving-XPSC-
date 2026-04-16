ef53767c35812dd3fed51e1129f89bfeb172ae58
mdnaim2004 <naim.cse2004@gmail.com>
2026-04-16 19:51:26 +0600

    Add test case for CF 1927 A problem solution

--- STL-1 Week 2/CF_1927_A.cpp
+++ STL-1 Week 2/CF_1927_A.cpp
@@ -10,6 +10,17 @@ int main(){
         cin >> n;
         string s;
         cin >> s;
+
+        int st = 0;
+        int end = 0;
+
+        for(int i=0; i<n; i++){
+            if(s[i] == 'B' && s[i] == 0)
+                st++;
+            if(s[i] == 'B' && s[i] != 0)
+                end++;
+        }
+        cout << abs(st-end)+1 << "\n";
         
     }
     return 0;
