7cfb4f78885a59d7ebe53b84d526a20d4ceb3ad5
mdnaim2004 <naim.cse2004@gmail.com>
2026-04-16 20:18:22 +0600

    Refactor CF 1927 A problem solution to calculate the first and last positions of 'B' and output the length of the subarray

--- STL-1 Week 2/CF_1927_A.cpp
+++ STL-1 Week 2/CF_1927_A.cpp
@@ -11,6 +11,22 @@ int main(){
         string s;
         cin >> s;
 
+        int fst = -1;
+        int lst = -1;
+
+        for(int i=0; i<n; i++){
+            if(s[i] == 'B'){
+                fst = i;
+                break;
+            }
+        }
+        for(int i=n-1; i>= 0; i--){
+            if(s[i] == 'B'){
+                lst = i;
+                break;
+            }
+        }
+        cout << lst -fst+1 << "\n";
         
     }
     return 0;
