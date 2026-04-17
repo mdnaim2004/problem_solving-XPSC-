41d498efc4f38d996b181889c6fda1fe171f73ea
mdnaim2004 <naim.cse2004@gmail.com>
2026-04-17 17:32:37 +0600

    Refactor CF 44 A solution to improve input handling and remove commented code

--- STL-1 Week 2/CF_44_A.cpp
+++ STL-1 Week 2/CF_44_A.cpp
@@ -4,8 +4,16 @@ using namespace std;
 int main(){
     int n;
     cin >> n;
-    while(n--){
-        
+    // while(n--){
+    //     string s;
+    //     getline(cin, s);
+    //     cout << s << endl;
+    // }
+    string s;
+    for(int i=0; i<=n; i++){
+        getline(cin, s);
+        cout << s << endl;
     }
+    
     return 0;
 }
