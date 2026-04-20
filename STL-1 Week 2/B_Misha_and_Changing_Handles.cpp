6287a3aa9c6c77103f1d982cf28aaeded24ca583
mdnaim2004 <naim.cse2004@gmail.com>
2026-04-20 10:23:06 +0600

    Add configuration updates for Windows and implement B. Misha and Changing Handles problem

---
+++ STL-1 Week 2/B_Misha_and_Changing_Handles.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int q;
+    cin >> q;
+    while(q--){
+        string old,news;
+        cin >> old >> news;
+
+    }
+    return 0;
+}
