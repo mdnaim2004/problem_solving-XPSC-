7b63e483a6a55b4088323eab500553162ff9688d
mdnaim2004 <naim.cse2004@gmail.com>
2026-04-21 20:24:06 +0600

    Fix implementation of CF 903 C problem by correcting input handling and logic for calculating maximum frequency

--- STL-1 Week 2/CF_903_C.cpp
+++ STL-1 Week 2/CF_903_C.cpp
@@ -2,8 +2,23 @@
 using namespace std;
 
 int main(){
-    int t;
-    cin >> t;
-    
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }    
+    map<int, int>mp;
+    for(int i=0; i<n; i++){
+        int val = arr[i];
+        mp[val]++;
+    }
+    int mx = 0;
+    for(auto [val, count] : mp){
+        mx = max(count, mx);
+    }
+    cout << mx << "\n";
+
     return 0;
 }
