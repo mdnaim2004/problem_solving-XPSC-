c7ad5ac4e367f402dc7275543218269d950d79ab
mdnaim2004 <naim.cse2004@gmail.com>
2026-04-24 23:03:23 +0600

    Add initial implementation for F-YetnotherrokenKeoard problem with input handling

---
+++ STL-1 Week 2/F-YetnotherrokenKeoard.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+
+    vector<string> s;
+    while(t--){
+        string s;
+        cin >> s;
+
+        for(int i=0; i<s.length(); i++){
+            
+        }
+        cout << "\n";
+    }
+    return 0;
+}
