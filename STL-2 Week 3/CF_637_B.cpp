49495d8c068ac9267bc68275cc814e3a431f27f4
mdnaim2004 <naim.cse2004@gmail.com>
2026-04-24 23:20:34 +0600

    Refactor CF 637 B solution for improved logic and input handling

--- STL-2 Week 3/CF_637_B.cpp
+++ STL-2 Week 3/CF_637_B.cpp
@@ -2,6 +2,22 @@
 using namespace std;
 
 int main(){
-    
+    int n, m;
+    cin >> n >>m;
+    vector<int> arr(n);
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    while(m--){
+        int q;
+        cin >>q;
+        
+        set<int> s;
+
+        for(int i = q; i<m; i++){
+            s.insert(arr[i]);
+        }
+        cout << s.size() << "\n";
+    }
     return 0;
 }
