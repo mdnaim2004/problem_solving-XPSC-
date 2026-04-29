7cc58bc7a301891fce9cd65fdf87fa9a450a14c4
mdnaim2004 <naim.cse2004@gmail.com>
2026-04-29 20:56:25 +0600

    Add initial implementation for Extra-Guests problem

---
+++ codechef div-4/Extra-Guests.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int x, y;
+    cin >> x >>y;
+    int s1 = (y-x)*150;
+    int s2 = x*100;
+    cout << s1+s2 <<"\n";
+    return 0;
+}
