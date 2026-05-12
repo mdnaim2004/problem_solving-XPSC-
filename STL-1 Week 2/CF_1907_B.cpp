08eb83e6b0b303d45bcd3f09a80ca5b12f58412d
Md Naim <naim.cse2004@gmail.com>
2026-05-12 22:49:23 +0600

    solve the another problem of the deily contibution

--- STL-1 Week 2/CF_1907_B.cpp
+++ STL-1 Week 2/CF_1907_B.cpp
@@ -8,6 +8,31 @@ int main(){
         string s;
         cin >> s;
 
+        vector<pair<char,int>> v;
+
+        for(char c: s){
+            if(c == 'b'){
+                for(int i=v.size() -1; i>=0; i--){
+                    if(islower(v[i].first)){
+                        v.erase(v.begin() + i);
+                        break;
+                    }
+                }
+            }
+            else if( c == 'B'){
+                for(int i=v.size()-1; i>=0; i--){
+                    if(isupper(v[i].first)){
+                        v.erase(v.begin() + i);
+                        break;
+                    }
+                }
+            }
+            else
+                v.push_back({c, 1});      
+        }
+        for( auto x : v)
+            cout << x.first;
+        cout << "\n";
     }
     return 0;
 }
